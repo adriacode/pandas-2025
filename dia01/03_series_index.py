a8c1b8c1a11be0c76e57c0c70069575ebff1cdbc
adriacode <adriacoding@gmail.com>
2025-05-19 18:46:08 -0300

    iloc, loc

---
+++ dia01/03_series_index.py
@@ -0,0 +1,60 @@
+# %%
+import pandas as pd
+
+idades = [
+    32, 38, 30, 30, 31,
+    35, 25, 29, 31, 37,
+    27, 23, 36, 33, 32,
+]
+
+series_idades = pd.Series(idades)
+series_idades
+
+# %%
+idades[0]
+series_idades[0]
+
+# %%
+series_idades[-1]
+
+# %%
+
+series_idades = series_idades.sort_values()
+series_idades
+
+# %%
+series_idades[0]
+
+# %%
+
+series_idades.iloc[0]
+series_idades.iloc[-1]
+
+
+# %%
+
+series_idades.iloc[:3]
+
+# %%
+
+idades = [
+    32, 38, 30, 30, 31,
+    35, 25, 29, 31, 37,
+    27, 23, 36, 33, 32,
+]
+
+indexs = [
+    "Téo", "Maria", "Jose", "Luis", "Ana",
+    "Nah", "Dani", "Mah", "Fer", "Nanda",
+    "Naty", "Nih", "Pedro", "Kozato", "Kozato",
+]
+
+series_idades = pd.Series(idades, index=indexs)
+series_idades
+
+# %% 
+series_idades.iloc[0]
+series_idades["Kozato"]
+
+# %%
+series_idades["Téo"]
