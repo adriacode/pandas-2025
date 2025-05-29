2f2d722822bd4684796ef8885e17f299d7b2aceb
adriacode <adriacoding@gmail.com>
2025-05-29 16:27:01 -0300

    ex 05 - parte 1

---
+++ exercicios/5/ex_05.py
@@ -0,0 +1,25 @@
+# %%
+import pandas as pd
+
+clientes = pd.read_csv("../../data/clientes.csv")
+transacoes = pd.read_csv("../../data/transacoes.csv")
+
+# %%
+# 05.01 - Crie uma coluna nova “twitch_points” que e resultado da multiplicação do saldo de pontos e a marcação da twitch
+clientes["twitch_points"] = clientes["qtdePontos"] * clientes["flTwitch"]
+clientes
+# %%
+# 05.02 - Aplique o log na coluna de saldo de pontos, criando uma coluna nova
+import numpy as np
+clientes["logQtdePontos"] = np.log(clientes["qtdePontos"]+1)
+clientes
+# %%
+# 05.03 - Crie uma coluna que sinalize se a pessoa tem vínculo com alguma (qualquer uma) plataforma de rede social.
+clientes["pelo_menos_uma_rede"] = (clientes["flEmail"] == 1) | (clientes["flTwitch"] == 1) | (clientes["flYouTube"] == 1) | (clientes["flBlueSky"] == 1) | (clientes["flInstagram"] == 1)
+clientes 
+# %%
+# 05.04 - Qual é o id de cliente que tem maior saldo de pontos? E o menor?
+clientes.sort_values(by="qtdePontos", ascending=False).head(1)["idCliente"]
+clientes.sort_values(by="qtdePontos", ascending=True).head(1)["idCliente"]
+# %%
+# 05.05 - Selecione a primeira transação diária de cada cliente.
